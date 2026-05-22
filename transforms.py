97dca907f90c7e8439b2b7dc01a3121edcbafc27
zxcdodster <knmescheryakov@edu.hse.ru>
2026-05-22 11:18:39 +0300

    add transforms.py

--- transforms.py
+++ transforms.py
@@ -3,11 +3,15 @@ from anomaly_detection.models import STLDetector
 import pandas as pd
 
 
-def linear_interpolate(df: pd.DataFrame):
+def linear_interpolate(df: pd.DataFrame, value_column: str = 'value_0'):
     """
     Линейно интерполирует значения в строках, где is_anomaly=1,
     используя соседние значения, где is_anomaly=0.
-    Если соседа нет с какой-то стороны, заменяет на 0.
+    Если соседа нет с какой-то стороны, заменяет на ближайшее известное значение.
+
+    Parameters:
+    df: DataFrame с колонками value_column и is_anomaly
+    value_column: название колонки со значениями для интерполяции
     """
     df = df.copy()
 
@@ -24,24 +28,25 @@ def linear_interpolate(df: pd.DataFrame):
             next_normal = df[normal_mask & (df.index > idx)]
 
             # Определяем значения для интерполяции
-            if len(prev_normal) == 0:
-                left_value = 0
-                left_idx = idx
+            if len(prev_normal) == 0 and len(next_normal) == 0:
+                # Если оба соседа отсутствуют, оставляем значение как есть
+                # или можно установить 0, но это крайний случай
+                df.loc[idx, value_column] = 0
+            elif len(prev_normal) == 0:
+                # Если нет левого соседа, используем ближайшее правое значение
+                right_value = next_normal.iloc[0][value_column]
+                df.loc[idx, value_column] = right_value
+            elif len(next_normal) == 0:
+                # Если нет правого соседа, используем ближайшее левое значение
+                left_value = prev_normal.iloc[-1][value_column]
+                df.loc[idx, value_column] = left_value
             else:
-                left_value = prev_normal.iloc[-1]['value_0']
+                # Есть оба соседа - делаем линейную интерполяцию
+                left_value = prev_normal.iloc[-1][value_column]
                 left_idx = prev_normal.index[-1]
-
-            if len(next_normal) == 0:
-                right_value = 0
-                right_idx = idx
-            else:
-                right_value = next_normal.iloc[0]['value_0']
+                right_value = next_normal.iloc[0][value_column]
                 right_idx = next_normal.index[0]
 
-            # Если оба соседа отсутствуют, значение = 0
-            if len(prev_normal) == 0 and len(next_normal) == 0:
-                df.loc[idx, 'value_0'] = 0
-            else:
                 # Преобразуем индексы в числовые значения для интерполяции
                 left_num = left_idx.timestamp()
                 right_num = right_idx.timestamp()
@@ -50,20 +55,24 @@ def linear_interpolate(df: pd.DataFrame):
                 # Линейная интерполяция
                 fraction = (current_num - left_num) / (right_num - left_num)
                 interpolated_value = left_value + fraction * (right_value - left_value)
-                df.loc[idx, 'value_0'] = interpolated_value
+                df.loc[idx, value_column] = interpolated_value
 
     return df
+
 
 def z_scores_anomaly_transform(series: TimeSeriesWrapper, system: AnomalyDetectionSystem) -> TimeSeriesWrapper:
     detector = AnomalyDetectionSystem.AVAILABLE_MODELS["STLDetector"](**system.detection_model_params)
 
-    z_scores =  detector.z_scores(series)
+    z_scores = detector.z_scores(series)
     model_output = detector(series)
 
     df = series.time_series_pd
     df["is_anomaly"] = model_output.is_anomaly
 
-    df = linear_interpolate(df)
+    # Добавляем z_scores в DataFrame
+    df["z_score"] = z_scores.values if hasattr(z_scores, 'values') else z_scores
+
+    # Интерполируем z_scores вместо value_0
+    df = linear_interpolate(df, value_column='z_score')
 
     return TimeSeriesWrapper(df)
-#DANYA
